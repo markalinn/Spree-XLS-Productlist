8c04736fc5da714c3c9c23140b79f0fea873f1e1
Mark Linn <marklinn@xwcsolutions.com>
2012-09-10 08:03:05 -0700

    addition of some fields for example

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -1,10 +1,9 @@
 # Make the Doc Helper available to all views
 ProductsController.class_eval do
   def xls_output
-    @products = Product.all
+    @products = Product.active(:order => 'name')
     render :layout => false
     headers['Content-Type'] = "application/vnd.ms-excel"
     headers['Content-Disposition'] = 'attachment; filename="products.xls"'
-    #TODO - Everything ;-)
   end
 end
